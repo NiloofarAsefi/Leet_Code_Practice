29e6a4997e032a21c2b8ab3d5152586d2f502620
Niloofar Asefi <nasefi@ucsc.edu>
2026-04-07 17:00:01 -0700

    add explanation

--- Github/leetcode-practice/28.Find_index_first_occurence_str.py
+++ Github/leetcode-practice/28.Find_index_first_occurence_str.py
@@ -9,3 +9,11 @@ class Solution:
              
         return -1
     
+# A sliding window means:
+#you look at a continuous part of the string or array
+#that part is called the window
+#then you move that window step by step
+
+#first window: haystack[0:m]
+#next window: haystack[1:1+m]
+#next window: haystack[2:2+m]
