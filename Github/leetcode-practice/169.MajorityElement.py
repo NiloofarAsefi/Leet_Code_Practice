81c689fa56728d356794530eae30d05f3696609e
Niloofar Asefi <nasefi@ucsc.edu>
2026-04-13 11:38:59 -0700

    update

--- Github/leetcode-practice/169.MajorityElement.py
+++ Github/leetcode-practice/169.MajorityElement.py
@@ -1,7 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         from collections import defaultdict
-        
+
         n = len(nums)
         output_d = defaultdict(int)
         for num in nums: 
@@ -12,3 +12,25 @@ class Solution:
                 return num 
 
 
+# space O(n)
+# time O(n)
+
+# Boyer-Moore Voting Algorithm:
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
+    
+# space O(1)
+# time O(n)
