e010ef938cfc1b0383ecc35263e30800fc1905c5
Niloofar Asefi <nasefi@ucsc.edu>
2026-04-15 17:11:18 -0700

    Linked List Cycle

---
+++ Github/leetcode-practice/141. Linked List Cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next: 
+            slow =slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True   
+        return False
+
+# Time Complexity: O(n)  
+# Space: O(1): You only use two pointers: slow and fast.
+# No extra data structure is used.
