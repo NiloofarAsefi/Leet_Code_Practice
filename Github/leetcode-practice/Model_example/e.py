acbea89071fe9721e02e4a82fda21b61618101b9
Niloofar Asefi <nasefi@ucsc.edu>
2026-04-29 13:48:36 -0700

    tensorflow

---
+++ Github/leetcode-practice/Model_example/e.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, activation="relu", input_shape=(10,)
+                          ),
+    tf.keras.layers.Dense(32, activation="relu"),
+    tf.keras.layers.Dense(1)
+                        
+])
+model.compile(optimizer="adam", loss="mse", metrics=["mae"])
+
+model.fit(X_train, y_train, epochs=20, batch_Size=32)
+
