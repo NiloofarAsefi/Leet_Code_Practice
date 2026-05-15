99bf6be416c6ec22179c39dc89f0aa93820521ec
Niloofar Asefi <nasefi@ucsc.edu>
2026-05-15 16:55:29 -0700

    update

--- Github/leetcode-practice/LeetCode_Easy/2006. Count Numb PairsAb Diff K.py
+++ Github/leetcode-practice/LeetCode_Easy/2006. Count Numb PairsAb Diff K.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
-                if np.abs(nums[i] - nums[j])==k:
+                if np.abs(nums[i] - nums[j])==k: # we can do abs alone without importing numpy, but I just wanted to use it for practice
                     output +=1
 
         return output
