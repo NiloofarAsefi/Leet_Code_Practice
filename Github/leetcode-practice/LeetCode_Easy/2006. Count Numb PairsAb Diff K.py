37015186295023585d2717f0d58620969c194366
Niloofar Asefi <nasefi@ucsc.edu>
2026-05-15 16:53:30 -0700

    2006

---
+++ Github/leetcode-practice/LeetCode_Easy/2006. Count Numb PairsAb Diff K.py
@@ -0,0 +1,13 @@
+import numpy as np
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        output=0 
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if np.abs(nums[i] - nums[j])==k:
+                    output +=1
+
+        return output
+
+                
